95dee8391a9e3fc4de8d22dd364b2acbebc2c1a1
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-16 22:21:23 -0400

    Create controllers

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/business', businessRouter);
