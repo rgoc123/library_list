2c260821a94aa1cfac91fc4491d271baec9a8fea
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-22 21:43:47 -0400

    Create bookinstance detail pages

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,17 +2,27 @@ var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res, next) {
-    BookInstance.find()
-      .populate('book')
-      .exec(function(err, list_bookinstances) {
-        if (err) {return next(err)}
-        res.render('bookinstance_list', { title: 'BookInstance List', bookinstance_list: list_bookinstances })
-      });
+  BookInstance.find()
+    .populate('book')
+    .exec(function(err, list_bookinstances) {
+      if (err) {return next(err)}
+      res.render('bookinstance_list', { title: 'BookInstance List', bookinstance_list: list_bookinstances })
+    });
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function(err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+        var err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('bookinstance_detail', { title: 'Book:', bookinstance: bookinstance});
+    });
 };
 
 // Display BookInstance create form on GET.
